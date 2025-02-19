aed91e02a49472b2c4702d8ef07a53206c26dda4
Virupakshi <virupakshivijji@gmail.com>
2025-02-19 15:51:59 +0530

    removed TypeORM used Prisma ORM for mapping

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username:'root',
-    password:'Amma@123',
-    database:'rt_feedback_db',
-    entities:[],
-    synchronize:true,
-  }),
-],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
