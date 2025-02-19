2ad113bf85f631b80c3cfee297a8b6190c4147d2
Virupakshi <virupakshivijji@gmail.com>
2025-02-19 11:55:51 +0530

    connected to mysql database locally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username:'root',
+    password:'Amma@123',
+    database:'rt_feedback_db',
+    entities:[],
+    synchronize:true,
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
